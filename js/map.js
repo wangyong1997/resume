134514d530756ebdc1036b4ce595f18151029cc6
zhlp <zhlp@videasoft.com>
2019-07-01 17:10:22 +0800

    1.0.0 初级版

---
+++ js/map.js
@@ -0,0 +1,22 @@
+jQuery(function($) {
+    "use strict";
+    /*
+        var map = new AMap.Map('map-canvas', {
+            zoom: 11, //级别
+            center: [108.8961410522, 34.1981730963], //中心点坐标
+            viewMode: '3D', //使用3D视图
+            resizeEnable: true,
+            mapStyle: 'amap://styles/dark',
+            pinch: 45
+        });
+
+        AMap.plugin([
+            'AMap.ControlBar',
+        ], function() {
+
+            // 添加 3D 罗盘控制
+            map.addControl(new AMap.ControlBar());
+        });
+    */
+
+});
